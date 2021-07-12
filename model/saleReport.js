45875dc589ff580f4566714f990f81115fda5603
imhamza1 <hamza.akram4635@gmail.com>
2021-07-13 01:08:24 +0500

    saleReport route added and completed

--- model/saleReport.js
+++ model/saleReport.js
@@ -1,15 +1,22 @@
 var mongoose = require("mongoose");
+function getDate() {
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getDate();
+  var year = dateObj.getUTCFullYear();
 
-var saleSchema = mongoose.Schema(
-  {
-    customerName: {
-      type: String,
-      default: "unknown",
-    },
-    data: [],
+  return year + "-" + month + "-" + day;
+}
+var saleSchema = mongoose.Schema({
+  customerName: {
+    type: String,
+    default: "unknown",
   },
-  { timestamps: true }
-);
+  costPriceTotal: { type: Number, default: 0 },
+  salePriceTotal: { type: Number, default: 0 },
+  data: [],
+  date: { type: String, default: new Date().getTime() / 1000 },
+});
 
 var Sale = mongoose.model("Sale", saleSchema);
 
--- routes/sales.js
+++ routes/sales.js
@@ -2,11 +2,37 @@ var express = require("express");
 var router = express.Router();
 var { Sale } = require("../model/saleReport");
 var { Stock } = require("../model/Stock");
+router.get("/", async (req, res) => {
+  let sales = await Sale.find().sort({ date: "desc" });
+  // console.log(sales);
+  res.send(sales);
+});
+router.post("/profit", async (req, res) => {
+  // console.log(req.body);
 
-router.get("/", async (req, res) => {});
+  let sales = await Sale.find({
+    date: {
+      $gte: new Date(req.body.start).setHours(00, 00, 00),
+      $lt: new Date(req.body.end).setHours(23, 59, 59),
+    },
+  });
+  // console.log(sales);
+  // console.log(sales);
+  res.send(sales);
+});
+router.get("/:id", async (req, res) => {
+  let sales = await Sale.findById(req.params.id);
+  // console.log(sales);
+  res.send(sales);
+});
 
 router.post("/", async (req, res) => {
-  let sale = new Sale({ customerName: req.body.name, data: req.body.data });
+  let sale = new Sale({
+    customerName: req.body.name ? req.body.name : "unknown",
+    costPriceTotal: req.body.costPriceTotal ? req.body.costPriceTotal : 0,
+    salePriceTotal: req.body.salePriceTotal ? req.body.salePriceTotal : 0,
+    data: req.body.data,
+  });
 
   for (let i = 0; i < sale.data.length; i++) {
     let dat = await Stock.findById(sale.data[i].id);
@@ -15,6 +41,11 @@ router.post("/", async (req, res) => {
   }
   await sale.save();
   res.send(sale);
+});
+
+router.delete("/del", async (req, res) => {
+  await Sale.remove({});
+  res.send("done");
 });
 
 module.exports = router;
